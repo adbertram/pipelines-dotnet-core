df303bdcb2fd596dd72345a4d5025f9589d776ff
Adam Bertram <adbertram@gmail.com>
2020-11-13 11:13:06 -0600

    Update configure_winrm.ps1

--- configure_winrm.ps1
+++ configure_winrm.ps1
@@ -37,13 +37,9 @@ function Create-Certificate {
         [string]$hostname
     )
 
-    # makecert ocassionally produces negative serial numbers
-    # which golang tls/crypto <1.6.1 cannot handle
-    # https://github.com/golang/go/issues/8265
-    $serial = Get-Random
-    .\makecert -r -pe -n CN=$hostname -b 01/01/2012 -e 01/01/2022 -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12 -# $serial 2>&1 | Out-Null
+    $cert = New-SelfSignedCertificate -Subject "CN=$hostname"
 
-    $thumbprint = (Get-ChildItem cert:\Localmachine\my | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1).Thumbprint
+    $thumbprint = $cert.Thumbprint
 
     if (-not $thumbprint) {
         throw "Failed to create the test certificate."
